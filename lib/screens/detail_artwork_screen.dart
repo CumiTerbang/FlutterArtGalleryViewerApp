5f5ec5e58a6f14497b00b461f1df9f9f2c8f51a1
Haryo Pramanto <gitarringsek@gmail.com>
2023-10-26 08:33:35 +0700

    update detail artwork screen

--- lib/screens/detail_artwork_screen.dart
+++ lib/screens/detail_artwork_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_art_gallery_viewer_app/data/helper/api_url.dart';
 import 'package:flutter_art_gallery_viewer_app/view_model/detail_artwork_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -17,7 +18,6 @@ class DetailArtworkScreen extends StatefulWidget {
 }
 
 class _DetailArtworkScreen extends State<DetailArtworkScreen> {
-
   @override
   void initState() {
     var viewModel = Provider.of<DetailArtworkViewModel>(context, listen: false);
@@ -32,13 +32,58 @@ class _DetailArtworkScreen extends State<DetailArtworkScreen> {
     return Consumer<DetailArtworkViewModel>(
         builder: (context, viewModel, child) {
       return Scaffold(
-        body: Center(
-          child: Text(
-            viewModel.getArtWorkItemModel?.title ?? "",
-            style: const TextStyle(fontSize: 40),
+        body: InkWell(
+          onTap: () {
+            viewModel.setShowDescription();
+          },
+          child: Stack(
+            children: <Widget>[
+              Image.network(
+                ApiUrl.getImagePath(
+                    viewModel.getArtWorkItemModel?.imageId ?? ''),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+              ...generateDesription(viewModel),
+            ],
           ),
         ),
       );
     });
+  }
+
+  List<Widget> generateDesription(DetailArtworkViewModel viewModel) {
+    var widgets = [
+      Positioned(
+          top: 24.0 + 16.0,
+          left: 16.0,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.5), shape: BoxShape.circle),
+            child: BackButton(
+              color: Colors.white,
+              onPressed: () {
+                viewModel.goBack();
+              },
+            ),
+          )),
+      Positioned(
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(24.0),
+          color: Colors.black.withOpacity(0.5),
+          child: Text(viewModel.getArtWorkDescription,
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+      ),
+    ];
+
+    return viewModel.getShowDescription ? widgets : [];
   }
 }
--- lib/view_model/detail_artwork_view_model.dart
+++ lib/view_model/detail_artwork_view_model.dart
@@ -1,12 +1,25 @@
 import 'dart:convert';
+import 'dart:ffi';
 
+import 'package:flutter_art_gallery_viewer_app/config/navigation_service.dart';
+import 'package:flutter_art_gallery_viewer_app/config/navigation_service_impl.dart';
+import 'package:flutter_art_gallery_viewer_app/config/service_locator.dart';
 import 'package:flutter_art_gallery_viewer_app/data/model/artwork_item_model.dart';
 import 'package:flutter_art_gallery_viewer_app/view_model/utils/utils.dart';
 
 class DetailArtworkViewModel extends BaseViewModel {
+  bool _showDescription = true;
   ArtworkItemModel? _artworkItemModel;
 
   ArtworkItemModel? get getArtWorkItemModel => _artworkItemModel;
+
+  String get getArtWorkDescription =>
+      "${getArtWorkItemModel?.artistDisplay ?? ""} (${getArtWorkItemModel?.dateDisplay ?? ""})\n\n" +
+      "\"${getArtWorkItemModel?.title}\"";
+
+  bool get getShowDescription => _showDescription;
+
+  NavigationService navigationService = locator.get<NavigationServiceImpl>();
 
   init(String artworkItemModelJson) {
     parseArtworkItemModelJson(artworkItemModelJson);
@@ -16,5 +29,14 @@ class DetailArtworkViewModel extends BaseViewModel {
   void parseArtworkItemModelJson(String artworkItemModelJson) {
     _artworkItemModel =
         ArtworkItemModel.fromJson(jsonDecode(artworkItemModelJson));
+  }
+
+  void goBack() {
+    navigationService.goBack();
+  }
+
+  void setShowDescription() {
+    _showDescription = !_showDescription;
+    notifyListeners();
   }
 }
